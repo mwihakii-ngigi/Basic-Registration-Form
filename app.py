c01d7462caa3d4c68eb693601f31699102ac3dd5
MWIHAKI NGIGI <ngigileah92@gmail.com>
2024-08-28 12:21:09 +0300

    Initial Commit

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
 class RegistrationApp(App):
     def build(self):
         self.title = "Registration Form"
         layout = BoxLayout(orientation = 'vertical', padding= 30, spacing=10)
+
+        head_label = Label()
 
         return layout
     
