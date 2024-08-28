9b0c20bcfc1267aaf6837b8a50497d2c205f0576
MWIHAKI NGIGI <ngigileah92@gmail.com>
2024-08-28 12:28:16 +0300

    Updates

--- app.py
+++ app.py
@@ -8,8 +8,19 @@ class RegistrationApp(App):
         self.title = "Registration Form"
         layout = BoxLayout(orientation = 'vertical', padding= 30, spacing=10)
 
-        head_label = Label()
+        head_label = Label(text="Python User Registration App", font_size=26, bold=True, height=40)
 
+        # Adding label
+        name_label = Label(text="Name:", font_size=18)
+
+        email_label = Label(text="Email:", font_size=18)
+
+        passwod_label = Label(text="Password:", font_size=18)
+
+        confirm_label = Label(text="Confirm Password:", font_size=18)
+
+
+        layout.add_widget(head_label)
         return layout
     
 
